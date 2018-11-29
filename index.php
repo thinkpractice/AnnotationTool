9f6db0d5bab69a613500d16c0583410d697685cc
Tim De Jong <tjadejong@thinkpractice.nl>
2018-11-29 16:20:35 +0100

    Merged index.php and index.html and do rerouting there.

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+
+require 'vendor/autoload.php';
+
+$router = new AltoRouter();
+$router->setBasePath('/AnnotationTool');
+
+// setup routes here
+$router->map('GET', '/get_images/', function(){
+    require __DIR__ . '/Images.php';
+});
+
+// match current request url
+$match = $router->match();
+
+// call closure or throw 404 status
+if( $match && is_callable( $match['target'] ) ) 
+{
+	call_user_func_array( $match['target'], $match['params'] ); 
+} 
+else 
+{
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
