2b2f95e8cf18d2ff9a9b477a4137205aa1bc6690
Tim De Jong <tjadejong@thinkpractice.nl>
2018-11-29 13:11:30 +0100

    Added routing in RestServices

--- RestService.php
+++ RestService.php
@@ -2,6 +2,23 @@
 
 $router = new AltoRouter();
 
+// setup routes here
+$router->map('GET', '/images/', function(){
+    require __DIR__ . 'Images.php';
+});
 
+// match current request url
+$match = $router->match();
+
+// call closure or throw 404 status
+if( $match && is_callable( $match['target'] ) ) 
+{
+	call_user_func_array( $match['target'], $match['params'] ); 
+} 
+else 
+{
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
 
 ?>
