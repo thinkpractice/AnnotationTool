5bba0adbcdd345b1ab363d8795db03516791a8d6
Tim De Jong <tjadejong@thinkpractice.nl>
2018-12-20 15:16:39 +0100

    More work on database structure

--- database.sql
+++ database.sql
@@ -6,7 +6,7 @@ create table users
     constraint pk_users primary key (user_id)
 );
 
-create table sessions
+create table user_sessions
 (
     session_id int not null auto_increment,
     session_uuid varchar(36),
@@ -18,6 +18,40 @@ create table content
     content_id int not null auto_increment,
     filename varchar(255) not null,
     constraint pk_content primary key (content_id)
+);
+
+create table project_type
+(
+    project_type_id int not null,
+    project_type_name varchar(255) not null,
+    constraint pk_project_type primary key (project_type_id)
+);
+
+create table project
+(
+    project_id int not null auto_increment,
+    project_type_id int not null,
+    project_name varchar(255) not null,
+    constraint pk_ primary key (project_id),
+    constraint fk_annotation_annotation_type foreign key (project_type_id) references annotation_type(project_type_id)
+);
+
+create table annotation_categories
+(
+    category_id int not null auto_increment,
+    category_name varchar(255) not null,
+    constraint pk_annotation_categories primary key (category_id)
+);
+
+create table content_annotation
+(
+    user_id int not null,
+    content_id int not null,
+    category_id int not null,
+    constraint pk_content_annotation primary key (user_id, content_id, category_id),
+    constraint fk_content_annotation_user_id foreign key (user_id) references users(user_id),
+    constraint fk_content_annotation_content_id foreign key (content_id) references content(content_id),
+    constraint fk_content_annotation_category_id foreign key (category_id) references annotation_categories(category_id)
 );
 
 create table user_session
