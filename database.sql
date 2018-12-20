7482cd077ac593fbaf0bc83f5fb8a0078e9cf7bf
Tim De Jong <tjadejong@thinkpractice.nl>
2018-12-20 12:03:18 +0100

    Small changes

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ create table users
 (
     user_id int not null auto_increment,
     user_name varchar(255),
-    password varchar(255),
+    user_password varchar(255),
     constraint pk_users primary key (user_id)
 );
 
@@ -13,6 +13,13 @@ create table sessions
     constraint pk_sessions primary key (session_id)
 );
 
+create table content
+(
+    content_id int not null auto_increment,
+    filename varchar(255) not null,
+    constraint pk_content primary key (content_id)
+);
+
 create table user_session
 (
     user_id int not null,
@@ -20,11 +27,4 @@ create table user_session
     constraint pk_user_session primary key (user_id, session_id),
     constraint fk_user_user_session foreign key (user_id) references users(user_id),
     constraint fk_session_user_session foreign key (session_id) references sessions(session_id)
-);
-
-create table content
-(
-    content_id int not null auto_increment,
-    filename varchar(255) not null,
-    constraint pk_content primary key (content_id)
 );
